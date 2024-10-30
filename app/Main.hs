986df13f47d49a89102f6d86713b2b8a8bfe7f3e
Vinicius de Lima <vinicius.lima.115@ufrn.edu.br>
2024-10-30 18:39:31 -0300

    fix(getSourceCodeFiles): correct check for ordinary file

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,7 @@ module Main where
 import Control.Monad (filterM, forM, when)
 import GHC.OldList (isSuffixOf)
 import SlocCLIParser (files, help, helpMessage, parseCommandLineArgs, recursive)
-import System.Directory (doesDirectoryExist, listDirectory)
+import System.Directory (doesDirectoryExist, doesFileExist, listDirectory)
 import System.Environment (getArgs)
 import System.Exit (exitFailure, exitSuccess)
 import System.FilePath ((</>))
@@ -29,15 +29,19 @@ listFilesRecursiverly :: FilePath -> IO [FilePath]
 listFilesRecursiverly filePath = do
   dirContents <- listDirectory filePath
   let fullPaths = map (filePath </>) dirContents
-  commonFiles <- filterM (fmap not . doesDirectoryExist) fullPaths
+  commonFiles <- filterM doesFileExist fullPaths
   directories <- filterM doesDirectoryExist fullPaths
   nestedFiles <- forM directories listFilesRecursiverly
   return $ commonFiles ++ concat nestedFiles
 
 getSourceCodeFiles :: Bool -> FilePath -> IO [FilePath]
 getSourceCodeFiles recursively filePath = do
-  commonFiles <- if recursively then listFilesRecursiverly filePath else listDirectory filePath
-  filterM (return . isSourceCodeFile) commonFiles
+  isDir <- doesDirectoryExist filePath
+  if isDir
+    then do
+      commonFiles <- if recursively then listFilesRecursiverly filePath else listDirectory filePath
+      filterM (return . isSourceCodeFile) commonFiles
+    else if isSourceCodeFile filePath then return [filePath] else return []
 
 -- Count the lines of
 -- Display his list based on the sort configuration.
